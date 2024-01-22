581433aaf0f0052f21f2aa05bca815cfc1060699
estebonbon <estesarmiento1@gmail.com>
2024-01-22 15:03:49 -0500

    Added disappear button

--- js/script.js
+++ js/script.js
@@ -3,8 +3,21 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait come back!";
+        button.classList.add("disappear");
+        //console.log("Yes");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, Cat!"
+        button.classList.remove("disappear");
+        //console.log("No");
+    }
+
+   
 });
 
 
 //console.log(cat);
+//cat.classList.add("show");
